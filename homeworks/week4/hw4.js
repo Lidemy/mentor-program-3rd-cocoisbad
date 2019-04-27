5aaefc1ac15b4791471b3b25e2bc6c4dc8f90a27
cocoisbad <myps60410@gmail.com>
2019-04-27 23:56:13 +0800

    'add'

---
+++ homeworks/week4/hw4.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+request(
+  {
+    url: 'https://api.twitch.tv/helix/games/top',
+    headers: {
+      'Client-ID': 'cht4rbpb659n4ystrmk3k6dwy08h66',
+    },
+  },
+
+  (error, response, body) => {
+    const json = JSON.parse(body);
+    for (let i = 0; i < json.data.length; i += 1) {
+      console.log(json.data[i].id, json.data[i].name);
+    }
+  },
+);
