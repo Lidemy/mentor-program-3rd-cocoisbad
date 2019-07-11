9ae8e9908ab440f73ee39bde04960d9e99ca5994
cocoisbad <myps60410@gmail.com>
2019-07-12 00:11:26 +0800

    'add'

--- homeworks/week12/hw1-2/delete.php
+++ homeworks/week12/hw1-2/delete.php
@@ -1,12 +1,37 @@
 <?php
   require_once('./conn.php');
-  $id = $_GET['id'];
+	if(!isset($_COOKIE["member_id"])) {
+	      die("not login");
+	  } else {
+	    $id = $_GET['id'];
+        $stmt = $conn->prepare("SELECT 
+          cocoisbad_comments.id,
+          cocoisbad_users_certificate.username
+           FROM cocoisbad_comments 
+           JOIN cocoisbad_users_certificate ON cocoisbad_comments.user_id = cocoisbad_users_certificate.id
+           WHERE cocoisbad_comments.id=?");
+          $stmt->bind_param("s", $id);
+          $stmt->execute();
+          $result = $stmt->get_result();
+          $row = $result->fetch_assoc();
+          
 
-  $stmt = $conn->prepare("DELETE FROM cocoisbad_comments WHERE id=?");
-    $stmt->bind_param("s", $id);
-  if($stmt->execute()) {
-  	header('Location: ./index.php?page=1');
-  } else {
-  	echo "failed:" . $conn->error;
-  }
+        $stmt = $conn->prepare("SELECT * from cocoisbad_users_certificate WHERE id=?");
+        $stmt->bind_param("s", $_COOKIE["member_id"]);
+        $stmt->execute();
+        $result2 = $stmt->get_result();
+        $row2 = $result2->fetch_assoc();
+        
+	      if($row2['username'] === $row['username']) {
+	          $stmt = $conn->prepare("DELETE FROM cocoisbad_comments WHERE id=?");
+	          $stmt->bind_param("s", $id);
+	          if($stmt->execute()) {
+	            header('Location: ./index.php?page=1');
+	          } else {
+	            echo "failed:" . $conn->error;
+	          }
+	      } else {
+	        echo ("這不是您的文章");
+	      }
+	  }
 ?>
